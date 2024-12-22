d908a53806f3bfddfc1583994aa6c0e48ec7b77b
unknown <shubhamrathoree123@gmail.com>
2024-12-22 22:43:55 +0530

    toggle2

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectView from "./pages/ProjectView";
 import Footer from "./pages/miniComponents/Footer";
 import { Toaster } from 'sonner'
+import ModeToggle from "./components/ModeToggle";
 
 
 export default function App() {
